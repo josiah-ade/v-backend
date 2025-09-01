08d02630221691c7fb01a8cef1fb5ffeaa5b40d1
Adelowo Babatunde <babatundeadelowo99@gmail.com>
2025-09-02 00:22:15 +0100

    added chat gateway

--- src/api/market-place/chat/entities/chat.entity.ts
+++ src/api/market-place/chat/entities/chat.entity.ts
@@ -1,4 +1,65 @@
-import { UserEntity } from '@/api/user/entities/user.entity';
+// import { UserEntity } from '@/api/user/entities/user.entity';
+// import { Uuid } from '@/common/types/common.type';
+// import { AbstractEntity } from '@/database/entities/abstract.entity';
+// import {
+//   Entity,
+//   JoinColumn,
+//   ManyToOne,
+//   OneToMany,
+//   OneToOne,
+//   PrimaryGeneratedColumn,
+//   Relation,
+// } from 'typeorm';
+// import { MessageEntity } from './message.entity';
+// import { ProductEntity } from '../../product/entities/product.entity';
+// import { BidEntity } from '../../bid/entities/bid.entity';
+
+// @Entity('chat_room')
+// export class ChatRoomEntity extends AbstractEntity {
+//   constructor(data?: Partial<ChatRoomEntity>) {
+//     super();
+//     Object.assign(this, data);
+//   }
+
+//   @PrimaryGeneratedColumn('uuid', {
+//     primaryKeyConstraintName: 'PK_chat_room_id',
+//   })
+//   id!: Uuid;
+
+//   @ManyToOne(() => ProductEntity, (product) => product.chatRooms, {
+//     onDelete: 'CASCADE',
+//   })
+//   @JoinColumn({
+//     name: 'product_id',
+//     referencedColumnName: 'id',
+//     foreignKeyConstraintName: 'FK_chat_room_product_id',
+//   })
+//   product!: Relation<ProductEntity>;
+
+//   @ManyToOne(() => UserEntity)
+//   @JoinColumn({ name: 'owner_id' })
+//   owner!: UserEntity;
+
+//   @ManyToOne(() => UserEntity)
+//   @JoinColumn({ name: 'bidder_id' })
+//   bidder!: UserEntity;
+
+//   @OneToMany(() => MessageEntity, (msg) => msg.chatRoom)
+//   messages!: Relation<MessageEntity[]>;
+
+//   @OneToOne(() => MessageEntity, { nullable: true })
+//   @JoinColumn({ name: 'last_message_id' })
+//   lastMessage?: Relation<MessageEntity>;
+
+//   @OneToMany(() => BidEntity, (bid) => bid.chatRoom)
+//   bids!: Relation<BidEntity[]>;
+// }
+
+
+import { type UserEntity } from '@/api/user/entities/user.entity'; 
+import { type ProductEntity } from '../../product/entities/product.entity'; 
+import { type MessageEntity } from './message.entity'; 
+import { type BidEntity } from '../../bid/entities/bid.entity'; 
 import { Uuid } from '@/common/types/common.type';
 import { AbstractEntity } from '@/database/entities/abstract.entity';
 import {
@@ -10,9 +71,6 @@ import {
   PrimaryGeneratedColumn,
   Relation,
 } from 'typeorm';
-import { MessageEntity } from './message.entity';
-import { ProductEntity } from '../../product/entities/product.entity';
-import { BidEntity } from '../../bid/entities/bid.entity';
 
 @Entity('chat_room')
 export class ChatRoomEntity extends AbstractEntity {
@@ -26,9 +84,11 @@ export class ChatRoomEntity extends AbstractEntity {
   })
   id!: Uuid;
 
-  @ManyToOne(() => ProductEntity, (product) => product.chatRooms, {
-    onDelete: 'CASCADE',
-  })
+  @ManyToOne(
+    () => require('../../product/entities/product.entity').ProductEntity, 
+    (product: ProductEntity) => product.chatRooms,
+    { onDelete: 'CASCADE' },
+  )
   @JoinColumn({
     name: 'product_id',
     referencedColumnName: 'id',
@@ -36,21 +96,31 @@ export class ChatRoomEntity extends AbstractEntity {
   })
   product!: Relation<ProductEntity>;
 
-  @ManyToOne(() => UserEntity)
+  @ManyToOne(
+    () => require('@/api/user/entities/user.entity').UserEntity, 
+  )
   @JoinColumn({ name: 'owner_id' })
   owner!: UserEntity;
 
-  @ManyToOne(() => UserEntity)
+  @ManyToOne(
+    () => require('@/api/user/entities/user.entity').UserEntity, 
+  )
   @JoinColumn({ name: 'bidder_id' })
   bidder!: UserEntity;
 
-  @OneToMany(() => MessageEntity, (msg) => msg.chatRoom)
+  @OneToMany(
+    () => require('./message.entity').MessageEntity, 
+    (msg: MessageEntity) => msg.chatRoom,
+  )
   messages!: Relation<MessageEntity[]>;
 
-  @OneToOne(() => MessageEntity, { nullable: true })
+  @OneToOne(() => require('./message.entity').MessageEntity, { nullable: true }) 
   @JoinColumn({ name: 'last_message_id' })
   lastMessage?: Relation<MessageEntity>;
 
-  @OneToMany(() => BidEntity, (bid) => bid.chatRoom)
+  @OneToMany(
+    () => require('../../bid/entities/bid.entity').BidEntity, 
+    (bid: BidEntity) => bid.chatRoom,
+  )
   bids!: Relation<BidEntity[]>;
 }
