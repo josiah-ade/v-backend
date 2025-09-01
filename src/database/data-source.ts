c4d2dc3d218cfd80363ec02249824e9b936df9fd
Adelowo Babatunde <babatundeadelowo99@gmail.com>
2025-09-01 21:47:46 +0100

    added chat gateway

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -6,13 +6,13 @@ import { SeederOptions } from 'typeorm-extension';
 export const AppDataSource = new DataSource({
   type: process.env.DATABASE_TYPE,
   url: process.env.DATABASE_URL,
-  host: process.env.DATABASE_HOST,
-  port: process.env.DATABASE_PORT
-    ? parseInt(process.env.DATABASE_PORT, 10)
-    : 5432,
-  username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
+  // host: process.env.DATABASE_HOST,
+  // port: process.env.DATABASE_PORT
+  //   ? parseInt(process.env.DATABASE_PORT, 10)
+  //   : 5432,
+  // username: process.env.DATABASE_USERNAME,
+  // password: process.env.DATABASE_PASSWORD,
+  // database: process.env.DATABASE_NAME,
   synchronize: process.env.DATABASE_SYNCHRONIZE === 'true',
   dropSchema: false,
   keepConnectionAlive: true,
