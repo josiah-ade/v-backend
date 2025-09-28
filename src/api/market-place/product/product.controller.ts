2a8691c676a1fc9c5da226379a16205458cf6782
josiah-ade <fhibiblog@gmail.com>
2025-09-28 21:00:21 +0100

    added new fields

--- src/api/market-place/product/product.controller.ts
+++ src/api/market-place/product/product.controller.ts
@@ -61,6 +61,7 @@ export class ProductController {
     type: GetProductResDto,
     summary: 'Get Single Product Detail',
   })
+  @UseInterceptors()
   async getProduct(
     @Param('id') id: Uuid,
     @CurrentUser('id') userId: Uuid,
